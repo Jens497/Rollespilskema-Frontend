81de47e782c7c502f74b083d54edd857c9143cce
Mads Møller Jensen <mads1606@gmail.com>
2024-02-06 13:19:45 +0100

    Add license to modified MIT code

--- src/util/TypeKeysUtil.ts
+++ src/util/TypeKeysUtil.ts
@@ -5,6 +5,31 @@
 /// Any code not from the original source has been annotated with an author
 /// if no author is annotated it can be assumed to be from react-hook-form.
 
+
+// The original license is included here in case this counts as a substantive portion of their software:
+//
+// MIT License
+//
+// Copyright (c) 2019-present Beier(Bill) Luo
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
 /* eslint-disable @typescript-eslint/no-explicit-any */
 type Primitive = null | undefined | string | number | boolean | symbol | bigint;
 
